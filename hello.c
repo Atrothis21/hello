e01cf355faa3b974d42d89bd1f151989242a141d
Ethan <ewood17@huskers.unl.edu>
2021-08-24 21:27:13 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Ethan Wood
+ * Date: 2021/8/23
+ * 
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+    printf("Hello World!\n");
+
+    return 0;    
+ }
